a4ad8c4ff54c7a5ce86dedd9488ee18ef227e395
Le Trong Dat <datlt2306@gmail.com>
2022-03-14 15:18:46 +0700

    feat: get products with api

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 import logo from './logo.svg'
 import './App.css'
 import ShowInfo from './components/ShowInfo'
@@ -8,13 +9,22 @@ function App() {
     name: "Dat",
     age: 22
   });
-
+  const [products, setProducts] = useState<ProductType[]>([]);
   const [count, setCount] = useState<number>(0);
-
+  
+  useEffect(() => {
+     const getProducts = async () => {
+        const { data } = await axios.get('http://localhost:8000/api/products');
+        setProducts(data);
+     }
+     getProducts();
+  },[])
   return (
     <div className="App">
       {count} <button onClick={() => setCount(count + 1)}>Click</button>
       <ShowInfo info={info}/>
+      <hr />
+      {products.map(item => <div>{item.name}</div>)}
     </div>
   )
 }
