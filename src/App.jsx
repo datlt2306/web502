b5ccdc9aaddaf719f0bde4d7d710fd713cc6fc23
Le Trong Dat <datlt2306@gmail.com>
2022-03-14 11:18:11 +0700

    feat: lifecycle

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,42 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Box from './components/Box'
 
+// class App extends React.Component{
+//    state = { // 1
+//      count: 10
+//    }
+//    componentDidMount(){ // 3 -> Hàm này chỉ được 1 lần sau khi render
+//       document.title = `Count: ${this.state.count}`;
+//    }
+//    componentDidUpdate(){
+//     document.title = `Count: ${this.state.count}`;
+//    }
+//    render(){ // 2
+//      return <div>{this.state.count}
+//       <button onClick={() => this.setState({count: this.state.count + 1})}>Click</button>
+//      </div>
+//    }
+// }
+// export default App;
 
-const ShowInfo = (props) => {
-  return <div>Show {props.name}</div>
+const App = () => {
+  const [count,setCount] = useState(0); // 1
+  const [products, setProducts] = useState([]); // 1
+  useEffect(() => { // 3
+    // call api
+    async function getProducts(){
+        const response = await fetch('http://localhost:8000/api/products');
+        const data = await response.json();
+        setProducts(data);
+    }
+    getProducts();
+    // document.title = `Count : ${count}`
+  }, [])
+  return <div>{count}
+      <button onClick={() => setCount(count + 1)}>Click</button>
+      {products.map(item => item.name)}
+     </div>
 }
-
-function App(){
-  const producst = []
-  const [count, setCount] = useState(0);
-  const [color, setColor] = useState("green");
-  const [myStatus, setMyStatus] = useState(false);
-  const [products, setProducts] = useState([{id: 1, name: "A"}, {id: 2, name: "B"}, { id: 3, name: "C"}])
-  // remove Item
-  const removeItem = (id) => {
-    const newProducts = products.filter(item => item.id !== id);
-    setProducts(newProducts);
-  }
-  return <div>
-     <h2>Demo State basic </h2> <hr /> 
-      String:  <Box color={color} /><br />
-      Boolean: { myStatus ? "Da ket hon" : "Chua ket hon"} <br />
-      
-      <h2>Demo event</h2>
-      Number: {count} <br /> <button onClick={() => setCount(count+1)}>Click count</button>
-      <button onClick={() => setMyStatus(!myStatus)}>Toggled Status</button>
-      <br />
-      { myStatus && <div>
-        Arr: {products.map(item => <div>{item.name} 
-          <button onClick={() => removeItem(item.id) }>Delete</button></div>)}
-      </div>}
-      <hr />
-      
-  </div>
-}
-export default App
+export default App;
