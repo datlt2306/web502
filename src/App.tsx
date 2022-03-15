13b3f85defa8141e81e3ed37692cf7dd725399d6
Le Trong Dat <datlt2306@gmail.com>
2022-03-15 17:24:46 +0700

    feat: react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,37 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import ShowInfo from './components/ShowInfo'
+import { NavLink, Route, Routes } from 'react-router-dom'
+import Homepage from './pages/Homepage'
+import ProductPage from './pages/Product'
 
 function App() {
-  const [count, setCount] = useState<number>(0);
-  const [myName, setMyName] = useState<string>("Le Trong Dat");
-  const [status, setStatus] = useState<boolean>(false);
-  const [info, setInfo] = useState<{name: string, age: number}>({ name: "Dat", age: 20});
-  const [products, setProducts] = useState<{id: number, name: string}[]>([
-    {id: 1, name: "Product A"},
-    {id: 2, name: "Product B"},
-    {id: 3, name: "Product C"}
-  ]);
-
-
-  const removeItem = (id: number) => {
-    const newsProduct = products.filter(item => item.id !== id);
-    setProducts(newsProduct)
-  }
   return (
-    <div className="App">
-      Count: {count} <button onClick={() => setCount(count + 1)}>Click</button>
-      <hr />
-      Full Name: {myName} <button onClick={() => setMyName("Le Tung Lam")}>Change Name</button>
-      <hr />
-      Status: {status ? "True" : "False"}
-      <hr />
-      Info: {info.name} - {info.age}
-      <hr />
-      Products: { products.map(item => <div>{item.name} <button onClick={() => removeItem(item.id)}>Remove</button></div>)}
-      <hr />
-      Component: ShowInfo
-      <ShowInfo name="Dat" age={20}/>
+    <div className="container">
+      <header>
+        <ul>
+          <li><NavLink to="/">Home Page</NavLink></li>
+          <li><NavLink to="/product">Product</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li>
+        </ul>
+      </header>
+      <main>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="product" element={<ProductPage />}/>
+            <Route path="about" element={<h1>About Page</h1>}/>
+          </Routes>
+      </main>
     </div>
   )
 }
 
 export default App
-
-
-// function useState(state){
-//   return [state, function(){}];
-// }
-
-// const [firstValue, secondValue] = useState(10);
-// console.log(firstValue) // 10
-
-// secondValue(20); //
-
-// console.log(firstValue) // 20
-
-
+/**
+ * B1: npm i react-router-dom
+ * B2: wrapper các ứng dụng sử dụng react-router-dom : 
+ *    - Truy cập file main.tsx : 
+ *      + import { BrowserRouter } from 'react-router-dom';
+ *      + <BrowserRouter><App /></BrowserRouter>
+ * B3: Sử dụng Component Routes, Route, NavLink
+ */
