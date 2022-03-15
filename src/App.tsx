4402fe6bfdbf2b778a0e80e4fba3316eddaf3c12
Le Trong Dat <datlt2306@gmail.com>
2022-03-15 16:40:53 +0700

    Buổi 3

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,45 @@ import './App.css'
 import ShowInfo from './components/ShowInfo'
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [myName, setMyName] = useState("Le Trong Dat");
+  const [status, setStatus] = useState(false);
+  const [info, setInfo] = useState({ name: "Dat", age: 20});
+  const [products, setProducts] = useState([
+    {id: 1, name: "Product A"},
+    {id: 2, name: "Product B"},
+    {id: 3, name: "Product C"}
+  ])
   return (
     <div className="App">
+      Count: {count} 
+      <hr />
+      Full Name: {myName}
+      <hr />
+      Status: {status ? "True" : "False"}
+      <hr />
+      Info: {info.name} - {info.age}
+      <hr />
+      Products: { products.map(item => <div>{item.name}</div>)}
+      <hr />
+      Component: ShowInfo
       <ShowInfo name="Dat" age={20}/>
     </div>
   )
 }
 
 export default App
+
+
+// function useState(state){
+//   return [state, function(){}];
+// }
+
+// const [firstValue, secondValue] = useState(10);
+// console.log(firstValue) // 10
+
+// secondValue(20); //
+
+// console.log(firstValue) // 20
+
+
