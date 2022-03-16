cbeceb1512d306cd4116c4a3dca86b51b504e847
Le Trong Dat <datlt2306@gmail.com>
2022-03-16 14:59:18 +0700

    feat: Create layout admin & website

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,13 @@ import './App.css'
 import ShowInfo from './components/ShowInfo'
 import type { ProductType } from './types/product';
 import { list, remove } from './api/product';
-import { NavLink, Route, Routes } from 'react-router-dom';
+import { Navigate, NavLink, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Product from './pages/Product';
+import Dashboard from './pages/Dashboard';
+import ManagerProduct from './pages/ManagerProduct';
+import WebsiteLayout from './pages/layouts/WebsiteLayout';
+import AdminLayout from './pages/layouts/AdminLayout';
 function App() {
   const [products, setProducts] = useState<ProductType[]>([]); // 1
   // const [count, setCount] = useState<number>(0);
@@ -49,13 +55,24 @@ function App() {
         <ul>
           <li><NavLink to="/">Home page</NavLink></li>
           <li><NavLink to="/product">Product</NavLink></li>
-          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/admin/dashboard">Admin Dashboard</NavLink></li>
         </ul>
       </header>
       <main>
       <Routes>
-        <Route path="/" element={<h1>Home page</h1>} />
-        <Route path="product" element={<h1>Product page</h1>} />
+        {/* <Route path="/" element={<Home />} />
+        <Route path="product" element={<Product />} />
+        <Route path="/admin/dashboard" element={<Dashboard />} />
+        <Route path="/admin/product" element={<ManagerProduct />} /> */}
+        <Route path="/" element={<WebsiteLayout />}>
+            <Route index element={<Home />} />
+            <Route path="product" element={<Product />} />
+        </Route>
+        <Route path="admin" element={<AdminLayout />}> 
+          <Route index element={<Navigate to="dashboard"/>} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="product" element={<ManagerProduct />} />
+        </Route>
       </Routes>
       </main>
       
@@ -64,3 +81,8 @@ function App() {
 }
 
 export default App
+//B1: npm i react-router-dom
+//B2: sử dụng component <BrowserRouter> để wrapper <App /> trong file main.tsx
+//B3: Khai báo và sử dụng <Routes> trong app
+//B4: Khai báo sử dụng <Route> để định nghĩa các đường đẫn
+
