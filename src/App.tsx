e1e30ff1daf168250c4560f58d8b2b3de2b4bf73
Le Trong Dat <datlt2306@gmail.com>
2022-03-17 17:42:35 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import { Navigate, NavLink, Route, Routes } from 'react-router-dom'
+import { Navigate, NavLink, Route, Router, Routes } from 'react-router-dom'
 import Homepage from './pages/Homepage'
 import ProductPage from './pages/Product'
 import Header from './components/Header'
 import WebsiteLayout from './pages/layouts/WebsiteLayout'
 import AdminLayout from './pages/layouts/AdminLayout'
+import ProductDetail from './pages/ProductDetail'
 
 function App() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => { // 3 c
+    const getProducts = async () => {
+        const response = await fetch('http://localhost:8000/api/products/');
+        const data = await response.json();
+        setProducts(data);
+    }   
+    getProducts();
+}, []);
   return (
     <div className="container">
+      {products.map(item => <div>{item.name}</div>)}
         <Routes>
           <Route path="/" element={<WebsiteLayout />}>
               <Route index element={<Homepage />} />
               <Route path="product" element={<h1>Product Page</h1>}/>
+              <Route path="/product/:id" element={<ProductDetail />} />
           </Route>
           <Route path="admin" element={<AdminLayout />}>
               <Route index element={<Navigate to="dashboard" />} />
@@ -34,3 +46,5 @@ export default App
  *      + <BrowserRouter><App /></BrowserRouter>
  * B3: Sử dụng Component Routes, Route, NavLink
  */
+
+
