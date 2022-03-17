322170c39aaab6e79b4b8707dd56bf5f81373b2f
Le Trong Dat <datlt2306@gmail.com>
2022-03-17 12:36:57 +0700

    feat: Admin,website layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { useEffect, useState } from 'react'
-import { Routes, Route, NavLink } from 'react-router-dom';
+import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
 import logo from './logo.svg'
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./dashboard.css";
+
 import ShowInfo from './components/ShowInfo'
 import Product from './components/Product'
 import { list, remove } from './api/product';
 import axios from 'axios';
 import type { IProduct } from './types/product';
+import AdminLayout from './pages/layouts/AdminLayout';
+import WebsiteLayout from './pages/layouts/WebsiteLayout';
+import Dashboard from './pages/Dashboard';
+import ProductManager from './pages/layouts/ProductManager';
+import Home from './pages/Home';
 
 
 function App() {
@@ -49,10 +56,19 @@ function App() {
           </ul>
         </header>
         <main>
+          
           <Routes>
-            <Route path="/" element={<h1>Home Page</h1>} />
-            <Route path="product" element={products.map(item => <div>{item.name}</div>)} />
-            <Route path="about" element={<ShowInfo name="abc" age={10} />} />
+            <Route path="/" element={<WebsiteLayout />}>
+              <Route index element={<Home />} />
+              <Route path="product" element={<h1>Hien thi san pham</h1>} />
+              <Route path="about" element={<h1>About page</h1>} />
+            </Route>
+            
+            <Route path="admin" element={<AdminLayout />}>
+                <Route index element={<Navigate to="dashboard"/>} />
+                <Route path="dashboard" element={<Dashboard />} />
+                <Route path="products" element={<ProductManager />} />
+            </Route>
           </Routes>
         </main>
     </div>
