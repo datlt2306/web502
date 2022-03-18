34539ba42e96e7269bdabb3a9839303a6490e0a0
Le Trong Dat <datlt2306@gmail.com>
2022-03-18 10:27:09 +0700

    feat: add product

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css'
 import ShowInfo from './components/ShowInfo'
 
 import type { Product } from './types/product';
-import { list } from './api/product';
+import { add, list } from './api/product';
 import { Navigate, NavLink, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
@@ -13,6 +13,7 @@ import WebsiteLayout from './pages/layouts/WebsiteLayout';
 import Dashboard from './pages/Dashboard';
 import ProductManager from './pages/ProductManager';
 import ProductDetail from './pages/ProductDetail';
+import ProductAdd from './pages/ProductAdd';
 
 function App() {
   const [products, setProducts] = useState<{_id: number, name: string}[]>([])
@@ -23,7 +24,15 @@ function App() {
         setProducts(data);
     }
     getProducts();
-  }, [])
+  }, []);
+
+  const onHandleAdd = async (product: any) => {
+    console.log('app.js', product);
+    // api
+    const {data} = await add(product);
+    // reRender
+    setProducts([...products, data]);
+  }
   return (
     <div className="App">
         <header>
@@ -37,15 +46,12 @@ function App() {
           <Routes>
             <Route path="/" element={<WebsiteLayout />}>
                 <Route index element={<HomePage />} />
-                {/* Cách 1: để vào chi tiết sản phẩm 
-                <Route path="product">
-                  <Route element={<ProductPage />} />
-                  <Route path=":id" element={<ProductDetail />} />
-                </Route> */}
-                {/* Cách 2: để vào chi tiết sản phẩm */}
-                  <Route path="product" element={<ProductPage />}  />
+                  <Route path="product" element={<ProductPage/>}  />
                   <Route path="/product/:id" element={<ProductDetail />} />
-            </Route>
+                  <Route path="/product/add" element={<ProductAdd name="Dat" onAdd={onHandleAdd} />} />
+            </Route>'
+            
+            '
             <Route path="admin" element={<AdminLayout />}>
                 <Route index element={<Navigate to="dashboard" />} />
                 <Route path="dashboard" element={<Dashboard />} />
