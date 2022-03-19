f80444b8821a158a2a59587e4407b9b98f61bb82
Le Trong Dat <datlt2306@gmail.com>
2022-03-19 16:55:20 +0700

    feat: list product

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,48 @@ import logo from './logo.svg'
 import './App.css'
 import { Navigate, NavLink, Route, Router, Routes } from 'react-router-dom'
 import Homepage from './pages/Homepage'
-import ProductPage from './pages/Product'
-import Header from './components/Header'
 import WebsiteLayout from './pages/layouts/WebsiteLayout'
 import AdminLayout from './pages/layouts/AdminLayout'
 import ProductDetail from './pages/ProductDetail'
+import ProductManager from './pages/ProductManager';
+import { ProductType } from './pages/types/product'
+import { list } from './api/product'
 
 function App() {
-  const [products, setProducts] = useState([]);
-  useEffect(() => { // 3 c
-    const getProducts = async () => {
-        const response = await fetch('http://localhost:8000/api/products/');
-        const data = await response.json();
-        setProducts(data);
-    }   
-    getProducts();
-}, []);
+  const [count, setCount] = useState(0);
+  const [status, setStatus] = useState(false);
+
+  const [products, setProducts] = useState<ProductType[]>([]);
+
+  useEffect(() => {
+      const getProducts = async () => {
+            const { data } = await list();
+            setProducts(data);
+      }
+      getProducts();
+  }, [])
+  
   return (
+
     <div className="container">
-      {products.map(item => <div>{item.name}</div>)}
+      {count} <button onClick={() => setCount(count + 1)}>Click</button>
+      <button onClick={() => setStatus(true)}>Click</button>
+      <div>
+        {products.map(item => item.name)}
+      </div>
         <Routes>
           <Route path="/" element={<WebsiteLayout />}>
               <Route index element={<Homepage />} />
-              <Route path="product" element={<h1>Product Page</h1>}/>
-              <Route path="/product/:id" element={<ProductDetail />} />
+              <Route path="product">
+                <Route index element={<h1>Product Page</h1>} />
+                <Route path=":id" element={<ProductDetail />} />
+              </Route>
+              
           </Route>
           <Route path="admin" element={<AdminLayout />}>
               <Route index element={<Navigate to="dashboard" />} />
               <Route path="dashboard" element={<h1>Dashboard page</h1>} />
+              <Route path="product" element={<ProductManager products={products} />} />
           </Route>
         </Routes>
     </div>
@@ -38,13 +52,5 @@ function App() {
 }
 
 export default App
-/**
- * B1: npm i react-router-dom
- * B2: wrapper các ứng dụng sử dụng react-router-dom : 
- *    - Truy cập file main.tsx : 
- *      + import { BrowserRouter } from 'react-router-dom';
- *      + <BrowserRouter><App /></BrowserRouter>
- * B3: Sử dụng Component Routes, Route, NavLink
- */
 
 
