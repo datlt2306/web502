0b6c92f4a91c843b876b40c42864936818025c6c
Le Trong Dat <datlt2306@gmail.com>
2022-11-12 08:34:57 +0700

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React, { useState } from "react";
 import Todo from "./components/Todo";
-
+import { Routes, Route, Outlet } from "react-router-dom";
+import HomePage from "./pages/home";
+import AboutPage from "./pages/about";
+import LayoutWebsite from "./components/Layout/LayoutWebsite";
 function App() {
     return (
         <>
-            <Todo />
+            <Routes>
+                <Route path="/" element={<LayoutWebsite />}>
+                    <Route index element={<HomePage />} />
+                    <Route path="about" element={<AboutPage />} />
+                    <Route path="todo" element={<Todo />} />
+                </Route>
+                <Route path="*" element={<h1>Not Found</h1>} />
+            </Routes>
         </>
     );
 }
 
 export default App;
+
+// .on('/', () => {
+//     console.log('1')
+// })
---
+++ src/pages/home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+const HomePage = (props: Props) => {
+    return (
+        <div>
+            <main>
+                <h1>Home page</h1>
+            </main>
+        </div>
+    );
+};
+
+export default HomePage;
