b1e5faeca9071ed0ac0758f7aee6b4708356748a
Le Trong Dat <datlt2306@gmail.com>
2022-11-12 08:12:41 +0700

    validate form

--- src/components/AddTodo/index.tsx
+++ src/components/AddTodo/index.tsx
@@ -1,25 +1,28 @@
+import { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { ITodo } from "../../interfaces/todo";
 
-type Props = {
+type AddTodoProps = {
     onAdd: (todo: any) => void;
 };
 
-const AddTodo = (props: Props) => {
+const AddTodo = (props: AddTodoProps) => {
     const {
         register,
         handleSubmit,
         formState: { errors },
     } = useForm<ITodo>();
 
-    const onSubmit: SubmitHandler<ITodo> = (data) => {
-        props.onAdd(data);
-    };
+    const onSubmit: SubmitHandler<ITodo> = (data) => {};
     return (
         <div>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <h1>Thêm sản phẩm</h1>
-                <input type="text" {...register("title")} />
+                <input type="text" {...register("title", { required: true, minLength: 500 })} />
+                {errors.title && errors.title.type === "required" && (
+                    <p>Bắt buộc phải nhập trường này</p>
+                )}
+                {errors.title && errors.title.type === "minLength" && <p>Min length is 500</p>}
                 <input type="checkbox" {...register("completed")} /> completed
                 <br />
                 <button>Add</button>
