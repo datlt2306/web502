fee305305c56eae5f8ec457b32ffe67ad2012f0a
datlt2306 <datlt2306@gmail.com>
2024-03-05 14:49:38 +0700

    Buổi 1 - commit 1
    Init

--- index.ts
+++ index.ts
@@ -1,27 +1,8 @@
-
 const a: number = 10;
 const b: number = 20;
 
-const sum = (a: number, b: number): number => {
+const sum = (a, b) => {
+    if (typeof a !== "number" || typeof b !== "number") return 0;
     return a + b;
 };
-console.info(sum(a, b));
-
-// Object
-interface TypeInfo { name: string, age: number };
-const info: TypeInfo = { name: "Dat", age: 30 };
-const showInfo = (info: TypeInfo): TypeInfo => {
-    return info;
-}
-showInfo(info);
-// Array
-
-interface IProduct {
-    name: string;
-    price: number;
-}
-const arrNumber: number[] | string[] = [1, 2, 3, 4, 5];
-const products: IProduct[] = [
-    { name: "A", price: 10 }
-]
-console.log(arrNumber);
+console.log(sum(a, b));
