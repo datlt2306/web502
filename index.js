fc46e5fc766be2783541d574b0a9e7f11911305b
datlt2306 <datlt2306@gmail.com>
2024-03-05 12:56:02 +0700

    Buổi 1 - commit 1

---
+++ index.js
@@ -0,0 +1,6 @@
+var a = 10;
+var b = 20;
+var sum = function (a, b) {
+    return a + b;
+};
+console.info(sum(a, b));
---
+++ index.ts
@@ -0,0 +1,8 @@
+
+const a: number = 10;
+const b: number = 20;
+
+const sum = (a: number, b: number): number => {
+    return a + b;
+};
+console.info(sum(a, b));
