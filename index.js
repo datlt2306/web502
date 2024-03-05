50b1ada2c543f538df5f831517c6c293a109b483
datlt2306 <datlt2306@gmail.com>
2024-03-05 15:46:18 +0700

    WD18330 - Buổi 1 - commit 4
    Type

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-var a = 20;
-var b = 20;
-var sum = function (a, b) {
-    if (typeof a !== "number" || typeof b !== "number")
-        return 0;
-    return a + b;
-};
-console.log(sum(a, b));
+// const a: number = 20;
+// const b: number = 20;
+// const sum = (a: number, b: number): number => {
+//     if (typeof a !== "number" || typeof b !== "number") return 0;
+//     return a + b
+// };
+// console.log(sum(a, b));
 // Core type
-var myName = "Le Trong Dat";
-var myAge = 20;
-var isMarried = true;
-var myInfo = {
-    name: "Dat",
-    age: 20
+// const myName: string = "Le Trong Dat";
+// const myAge: number = 20;
+// const isMarried: boolean = true;
+// const myInfo: { name: string, age: number } = {
+//     name: "Dat",
+//     age: 20,
+// }
+// const showInfo = (user: { name: string, age: number }): string => {
+//     return `${user.name} - ${user.age}`
+// }
+// console.log(showInfo(myInfo))
+var arrTuple = ["Dat", 20];
+var arrNumber = [1, 2, 3, 4];
+var arrString = ["a", "b", "c"];
+var unionType = "Thành công";
+var products = [
+    { id: 1, name: "Iphone 12", price: 1000 },
+    { id: 3, name: "Iphone 13", price: 1000 },
+];
+var showProduct = function (data) {
+    if (!Array.isArray(data))
+        return 'Data is not array';
+    return data.map(function (item) { return "\n            <div>".concat(item.name, "</div>\n        "); }).join("");
 };
-var showInfo = function (user) {
-    return "".concat(user.name, " - ").concat(user.age);
-};
-console.log(showInfo(myInfo));
+showProduct(products);
--- index.ts
+++ index.ts
@@ -1,23 +1,48 @@
-const a: number = 20;
-const b: number = 20;
+// const a: number = 20;
+// const b: number = 20;
 
-const sum = (a: number, b: number): number => {
-    if (typeof a !== "number" || typeof b !== "number") return 0;
-    return a + b
-};
-console.log(sum(a, b));
+// const sum = (a: number, b: number): number => {
+//     if (typeof a !== "number" || typeof b !== "number") return 0;
+//     return a + b
+// };
+// console.log(sum(a, b));
 
 
 // Core type
 
-const myName: string = "Le Trong Dat";
-const myAge: number = 20;
-const isMarried: boolean = true;
-const myInfo: { name: string, age: number } = {
-    name: "Dat",
-    age: 20,
+// const myName: string = "Le Trong Dat";
+// const myAge: number = 20;
+// const isMarried: boolean = true;
+// const myInfo: { name: string, age: number } = {
+//     name: "Dat",
+//     age: 20,
+// }
+// const showInfo = (user: { name: string, age: number }): string => {
+//     return `${user.name} - ${user.age}`
+// }
+// console.log(showInfo(myInfo))
+const arrTuple: [string, number] = ["Dat", 20];
+const arrNumber: number[] = [1, 2, 3, 4];
+const arrString: string[] = ["a", "b", "c"];
+const unionType: "Thành công" | "Thất bại" = "Thành công";
+
+
+// type product: 
+type TypeProduct = {
+    id: number
+    name: string,
+    price: number
 }
-const showInfo = (user: { name: string, age: number }): string => {
-    return `${user.name} - ${user.age}`
+const products: TypeProduct[] = [
+    { id: 1, name: "Iphone 12", price: 1000 },
+    { id: 3, name: "Iphone 13", price: 1000 },
+]
+
+const showProduct = (data: TypeProduct[]): string => {
+    if (!Array.isArray(data)) return 'Data is not array';
+    return data.map((item: TypeProduct) => `
+            <div>${item.name}</div>
+        `).join("")
 }
-console.log(showInfo(myInfo))
+showProduct(products);
+
