25531a99acdd4225371f2cf27fb2581d401609c6
datlt2306 <datlt2306@gmail.com>
2024-03-05 14:43:19 +0700

    Buổi 1 - Commit 3

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ var sum = function (a, b) {
     return a + b;
 };
 console.info(sum(a, b));
+;
+var info = { name: "Dat", age: 30 };
+var showInfo = function (info) {
+    return info;
+};
+showInfo(info);
+var arrNumber = [1, 2, 3, 4, 5];
+var products = [
+    { name: "A", price: 10 }
+];
+console.log(arrNumber);
--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 
-const a: string = 10;
+const a: number = 10;
 const b: number = 20;
 
 const sum = (a: number, b: number): number => {
@@ -8,15 +8,20 @@ const sum = (a: number, b: number): number => {
 console.info(sum(a, b));
 
 // Object
-const info: { name: string, age: number } = { name: "Dat", age: 30 };
-const showInfo = (info: { name: string, age: number }): { name: string, age: number } => {
+interface TypeInfo { name: string, age: number };
+const info: TypeInfo = { name: "Dat", age: 30 };
+const showInfo = (info: TypeInfo): TypeInfo => {
     return info;
 }
 showInfo(info);
 // Array
-const arrNumber: number[] = [1, 2, 3, 4, 5];
-const products: { name: string, price: number }[] = [
+
+interface IProduct {
+    name: string;
+    price: number;
+}
+const arrNumber: number[] | string[] = [1, 2, 3, 4, 5];
+const products: IProduct[] = [
     { name: "A", price: 10 }
 ]
-
 console.log(arrNumber);
