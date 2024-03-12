b7dcf00408b40ae31f5931a9d2edc895e986b84b
datlt2306 <datlt2306@gmail.com>
2024-03-12 12:33:50 +0700

    WD18328 - Buổi 5

--- WD18328/src/App.tsx
+++ WD18328/src/App.tsx
@@ -1,21 +1,25 @@
-import { useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { IProduct } from "./interfaces/product";
+import axios from "axios";
 
 function App() {
-  const [count] = useState<number>(0);
-  const [color] = useState<string
-  >('red');
-  return (
-    <>
-      <div>
-      {count}
-      <div style={{ width: 200, height: 200, backgroundColor: color}}>123</div>
-        {/* Function: {showInfo({name: "Dat", age: 30})} */}
-        <hr />
-        {/* Component: <ShowInfo name="Dat" age={30}/> */}
-        </div>
-    </>
-  )
+    const [products, setProducts] = useState<IProduct[]>([]);
+    useEffect(() => {
+        (async () => {
+            const { data } = await axios.get(`http://localhost:8080/api/products`);
+            setProducts(data);
+        })();
+    }, []);
+    return (
+        <>
+            {products.map((item: IProduct, index) => (
+                <div key={index}>{item.name}</div>
+            ))}
+            <div></div>
+        </>
+    );
 }
-
-export default App
+// Truyền vào 1 hàm và có dấu {}
+// nếu truyền vào 1 hàm thì phải có dấu () => return 1 hàm
+export default App;
