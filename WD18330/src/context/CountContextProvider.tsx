2f4d7a5ca14a9edcafcd057c1ffffb6196516a87
datlt2306 <datlt2306@gmail.com>
2024-03-16 15:11:57 +0700

    WD18330 - Buổi 5

--- WD18330/src/context/CountContextProvider.tsx
+++ WD18330/src/context/CountContextProvider.tsx
@@ -1,15 +1,29 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useReducer, useState } from "react";
 
 type Props = {
     children: React.ReactNode;
 };
-
+function reducer(state: any, action: any) {
+    // action = { type: "INCREASE", payload: 10}
+    if (action.type === "INCREMENT") {
+        return { value: state.value + 1 };
+    }
+    if (action.type === "DECREMENT") {
+        return { value: state.value - 1 };
+    }
+    if (action.type === "INCREASE") {
+        return { value: state.value + action.payload };
+    }
+    return state;
+}
 export const CountContext = createContext({} as any);
 const CountContextProvider = ({ children }: Props) => {
-    const [count, setCount] = useState(0);
+    const [count, dispatch] = useReducer(reducer, {
+        value: 0,
+    });
     return (
         <div>
-            <CountContext.Provider value={{ count, setCount }}>{children}</CountContext.Provider>
+            <CountContext.Provider value={{ count, dispatch }}>{children}</CountContext.Provider>
         </div>
     );
 };
